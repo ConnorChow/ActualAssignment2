ea9515267e81278f6d8c390673157a4c5235d678
ConnorChow <connorjameschow@gmail.com>
2023-03-03 19:32:36 -0500

    THE REVOLUTION IS IMMINENT

--- Assets/FlipCardsManager.cs
+++ Assets/FlipCardsManager.cs
@@ -3,12 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class FlipCardsManager : MonoBehaviour {
+    //Textures for flipping cards
+    public Sprite mao;
+    public Sprite stalin;
+    public Sprite lenin;
+    public Sprite sankara;
+    public Sprite luxemburg;
+    public Sprite min;
+    public Sprite che;
+    public Sprite castro;
+    public Sprite redStar;
 
     GameObject[] cards = new GameObject[16];
-    Button[] buttons = new Button[16];
+    Trait[] traits = new Trait[16];
+    public bool[] flipped = new bool[16];
+
+    //boolean to indicate when the game is complete
+    bool complete = false;
+
+    //index of the pair of cards flipped by player, -1 is nothing selected
+    public int flipped1 = -1;
+    public int flipped2 = -1;
 
     int[] availableLib = new int[16] { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
     HashSet<int> available = new HashSet<int>();
@@ -26,16 +45,55 @@ public class FlipCardsManager : MonoBehaviour {
 
         for (int i = 0; i < cards.Length; i++) {
             cards[i] = GameObject.Find("Card (" + i + ")");
-            buttons[i] = cards[i].GetComponent<Button>();
             randomIndex = available.ElementAt(r.Next(available.Count));
-            characterLib[i] = availableLib[randomIndex];
+
+            traits[i] = cards[i].GetComponent<Trait>();
+            traits[i].characterInt = availableLib[randomIndex];
+            traits[i].index = i;
+
+            flipped[i] = false;
+
             available.Remove(randomIndex);
         }
+        timer = delay;
     }
+
+    public float delay = 2.0f;
+    float timer;
 
     // Update is called once per frame
     void Update() {
-        foreach (var button in buttons) {
+        if (flipped1 != -1 && flipped2 != -1) {
+            if (cards[flipped1].GetComponent<Trait>().characterInt == cards[flipped2].GetComponent<Trait>().characterInt) {
+                flipped[flipped1] = true;
+                flipped[flipped2] = true;
+                
+                flipped1 = -1;
+                flipped2 = -1;
+            } else {
+                timer -= Time.deltaTime;
+                if (timer <= 0) {
+                    cards[flipped1].GetComponent<Trait>().HideCard();
+                    cards[flipped2].GetComponent<Trait>().HideCard();
+
+                    flipped1 = -1;
+                    flipped2 = -1;
+
+                    timer = delay;
+                }
+            }
+        }
+        bool allFinished = true;
+        for (int i = 0; i < flipped.Length; i++) {
+            if (!flipped[i])
+                allFinished = false;
+        }
+        if (!complete && allFinished) {
+            complete = true;
+            GetComponent<AudioSource>().Play();
+        }
+        if (complete && Input.GetKeyDown("space")){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
--- Assets/Trait.cs
+++ Assets/Trait.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Trait : MonoBehaviour {
     public int characterInt;
+    public int index;
+
     public FlipCardsManager flipCardsManager;
     // Start is called before the first frame update
     void Start() {
@@ -13,5 +16,52 @@ public class Trait : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
+    }
+
+    public void OnButtonPress() {
+        if (flipCardsManager.flipped[index] == false) {
+            if (flipCardsManager.flipped1 == -1) {
+                flipCardsManager.flipped1 = index;
+                SetCharacter();
+            } else if (flipCardsManager.flipped2 == -1) {
+                flipCardsManager.flipped2 = index;
+                SetCharacter();
+            }
+        }
+    }
+    private void SetCharacter() {
+        Image img = GetComponent<Image>();
+        if (img != null) {
+            switch (characterInt) {
+                case 0:
+                    img.sprite = flipCardsManager.lenin;
+                    break;
+                case 1:
+                    img.sprite = flipCardsManager.luxemburg;
+                    break;
+                case 2:
+                    img.sprite = flipCardsManager.mao;
+                    break;
+                case 3:
+                    img.sprite = flipCardsManager.min;
+                    break;
+                case 4:
+                    img.sprite = flipCardsManager.sankara;
+                    break;
+                case 5:
+                    img.sprite = flipCardsManager.stalin;
+                    break;
+                case 6:
+                    img.sprite = flipCardsManager.castro;
+                    break;
+                case 7:
+                    img.sprite = flipCardsManager.che;
+                    break;
+            }
+        }
+    }
+    public void HideCard() {
+        Image img = GetComponent<Image>();
+        img.sprite = flipCardsManager.redStar;
     }
 }
